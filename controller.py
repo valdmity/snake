6e4bb9e7ce31b2bc40129b2b5ef0618d569a9ca1
VladSurov <vlsur@outlook.com>
2022-05-12 16:17:39 +0500

    controller updated

--- controller.py
+++ controller.py
@@ -6,15 +6,14 @@ from objects.vector import *
 class Controller:
     def __init__(self, game_engine):
         self.game_engine = game_engine
+        self.dictionary_direction_by_key = {
+            pygame.K_w: vector_down, pygame.K_s: vector_up,
+            pygame.K_d: vector_right, pygame.K_a: vector_left,
+            pygame.K_UP: vector_down, pygame.K_DOWN: vector_up,
+            pygame.K_RIGHT: vector_right, pygame.K_LEFT: vector_left}
 
     def handle_event(self, key):
-        if key == pygame.K_w:
-            self.game_engine.snake.change_dir(vector_down)
-        elif key == pygame.K_s:
-            self.game_engine.snake.change_dir(vector_up)
-        elif key == pygame.K_d:
-            self.game_engine.snake.change_dir(vector_right)
-        elif key == pygame.K_a:
-            self.game_engine.snake.change_dir(vector_left)
+        if key in self.dictionary_direction_by_key:
+            self.game_engine.snake.change_dir(self.dictionary_direction_by_key[key])
         elif key == pygame.K_e:
             self.game_engine.snake.eat()
